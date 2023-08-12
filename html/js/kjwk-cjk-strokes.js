2db564818dfa97028e46bb0d1df71ea2dfae202e
Danang Priabada <8danang8@gmail.com>
2023-08-13 00:44:56 +0700

    Add new js Japanese and Korean (CJK) scripts share a common background

---
+++ html/js/kjwk-cjk-strokes.js
@@ -0,0 +1,547 @@
+/*
+	Make changes to references and dictionaries that are generated from
+	third-party sources. These may include:
+	* kanji_defs (from KANJIDIC project dictionary)
+	* ijidoukun (shared kun reading and meaning - from dictionary cross-ref)
+	* radical_list (from Wikipedia radical list)
+	This allows for future changes and updates without losing these fixes.
+	In the HTML page, include this script after all the above scripts and definitions.
+	you can verify the kanji is jouyou or not by find the list here http://nihongo.monash.edu/jouyoukanji.html
+	if you not found the character kanji, you can find by unicode https://www.compart.com/en/unicode/, usually the char is CJK basic and compound strokes
+    read more about CJK character here https://en.wikipedia.org/wiki/Stroke_(CJK_character) or https://en.wiktionary.org/wiki/Appendix:Unicode/CJK_Strokes
+    because there is a lot of unicode we also add the another unicode for CJK Radicals Supplement https://en.wiktionary.org/wiki/Appendix:Unicode/CJK_Radicals_Supplement
+*/
+
+/* Usefull sites :
+    - https://www.yellowbridge.com/chinese/charsearch.php
+    - http://www.whiteagle.net/
+*/
+
+/*
+    The Chinese, Japanese and Korean (CJK) scripts share a common background, collectively known as CJK characters.
+    During the process called Han unification, the common (shared) characters were identified and named CJK Unified Ideographs.
+    As of Unicode 15.0, Unicode defines a total of 97,058 characters.
+*/
+//U+53F9
+kanji_defs['叹'] = {
+	"on_readings": ["イウ"],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-53F9"
+	],
+	"is_jouyou": false
+};
+//U+4FDE
+kanji_defs['俞'] = {
+	"on_readings": ["ユ"],
+	"kun_readings": ["しかり"],
+	"meanings": [
+		"CJK Unified Ideograph-4FDE"
+	],
+	"is_jouyou": false
+};
+//U+2ECC
+kanji_defs['⻌'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"walk radical (no. 162)"
+	],
+	"is_jouyou": false
+};
+//U+3452
+kanji_defs['㑒'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-3452"
+	],
+	"is_jouyou": false
+};
+//U+9FBA
+kanji_defs['龺'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-9FBA"
+	],
+	"is_jouyou": false
+};
+//U+31C0
+kanji_defs['㇀'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke T"
+	],
+	"is_jouyou": false
+};
+//U+31C1
+kanji_defs['㇁'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Wg"
+	],
+	"is_jouyou": false
+};
+//U+31D6
+kanji_defs['㇖'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Hg"
+	],
+	"is_jouyou": false
+};
+//U+31C8
+kanji_defs['㇈'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Hzwg"
+	],
+	"is_jouyou": false
+};
+//U+2E8A
+kanji_defs['⺊'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Radical Divination"
+	],
+	"is_jouyou": false
+};
+//U+20087
+kanji_defs['𠂇'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-20087"
+	],
+	"is_jouyou": false
+};
+//U+4E37
+kanji_defs['丷'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-4E37"
+	],
+	"is_jouyou": false
+};
+//U+9FB7
+kanji_defs['龷'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-9FB7"
+	],
+	"is_jouyou": false
+};
+//U+200CA
+kanji_defs['𠃊'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-200CA"
+	],
+	"is_jouyou": false
+};
+//U+5DDF
+kanji_defs['巟'] = {
+	"on_readings": ["コウ"],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-5DDF"
+	],
+	"is_jouyou": false
+};
+//U+5917
+kanji_defs['夗'] = {
+	"on_readings": ["エン", "オン"],
+	"kun_readings": ["ころがりうす"],
+	"meanings": [
+		"CJK Unified Ideograph-5917"
+	],
+	"is_jouyou": false
+};
+//U+2E95
+kanji_defs['⺕'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Radical Snout Two"
+	],
+	"is_jouyou": false
+};
+//U+21C2A
+kanji_defs['𡰪'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-21C2A"
+	],
+	"is_jouyou": false
+};
+//U+2626A
+kanji_defs['𦉪'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-2626A"
+	],
+	"is_jouyou": false
+};
+//U+52A1
+kanji_defs['务'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-52A1"
+	],
+	"is_jouyou": false
+};
+// Town and mound radicals (which look the same) were under multiple Unicode points
+//U+2ED6
+kanji_defs['⻖'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"mound radical left (no. 170)"
+	],
+	"is_jouyou": false
+};
+//U+2ECF
+kanji_defs['⻏'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"city radical right (no. 163)"
+	],
+	"is_jouyou": false
+};
+//U+9FB4
+kanji_defs['龴'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-9FB4"
+	],
+	"is_jouyou": false
+};
+//U+2ECA
+kanji_defs['⻊'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"foot radical (no. 157)"
+	],
+	"is_jouyou": false
+};
+//U+9FB0
+kanji_defs['龰'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-9FB0"
+	],
+	"is_jouyou": false
+};
+//U+2E88
+kanji_defs['⺈'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"knife radical ne (no. 18)"
+	],
+	"is_jouyou": false
+};
+//U+31C2
+kanji_defs['㇂'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Xg"
+	],
+	"is_jouyou": false
+};
+//U+31C3
+kanji_defs['㇃'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Bxg"
+	],
+	"is_jouyou": false
+};
+//U+31C4
+kanji_defs['㇄'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Sw"
+	],
+	"is_jouyou": false
+};
+//U+31C5
+kanji_defs['㇅'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Hzz"
+	],
+	"is_jouyou": false
+};
+//U+31C6
+kanji_defs['㇆'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Hzg"
+	],
+	"is_jouyou": false
+};
+//U+5179
+kanji_defs['兹'] = {
+	"on_readings": ["ジ", "シ"],
+	"kun_readings": ["しげる", "ます", "むしろ"],
+	"meanings": [
+		"CJK Stroke Hzg"
+	],
+	"is_jouyou": false
+};
+//U+2E8C
+kanji_defs['⺌'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"small radical one (no. 42)"
+	],
+	"is_jouyou": false
+};
+//U+2E8D
+kanji_defs['⺍'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"radical small two (no. 42)"
+	],
+	"is_jouyou": false
+};
+//U+39AE
+kanji_defs['㦮'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-39AE"
+	],
+	"is_jouyou": false
+};
+//U+91D2
+kanji_defs['釒'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-91D2"
+	],
+	"is_jouyou": false
+};
+//U+5F55
+kanji_defs['录'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-5F55"
+	],
+	"is_jouyou": false
+};
+//U+27607
+kanji_defs['𧘇'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-27607"
+	],
+	"is_jouyou": false
+};
+//U+2EBC
+kanji_defs['⺼'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"radical meat (no. 130)"
+	],
+	"is_jouyou": false
+};
+//U+31C7
+kanji_defs['㇇'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Hp"
+	],
+	"is_jouyou": false
+};
+//U+31C8
+kanji_defs['㇈'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Hzwg"
+	],
+	"is_jouyou": false
+};
+//U+21FE8
+kanji_defs['𡿨'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-21FE8"
+	],
+	"is_jouyou": false
+};
+//U-200D1
+kanji_defs['𠃑'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-200D1"
+	],
+	"is_jouyou": false
+};
+//U-4E5B
+kanji_defs['乛'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-4E5B"
+	],
+	"is_jouyou": false
+};
+//U+2EA7
+kanji_defs['⺧'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"cow radical (no. 93)"
+	],
+	"is_jouyou": false
+};
+//U+9FB9
+kanji_defs['龹'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-9FB9"
+	],
+	"is_jouyou": false
+};
+//U-43CD
+kanji_defs['䏍'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-43CD"
+	],
+	"is_jouyou": false
+};
+//U-200CC
+kanji_defs['𠃌'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CCJK Unified Ideograph-200CC"
+	],
+	"is_jouyou": false
+};
+//U-4E60
+kanji_defs['习'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-4E60"
+	],
+	"is_jouyou": false
+};
+//U-2010C
+kanji_defs['𠄌'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-2010C"
+	],
+	"is_jouyou": false
+};
+//U-24570
+kanji_defs['𤕰'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-24570"
+	],
+	"is_jouyou": false
+};
+//U-2123C
+kanji_defs['𡈼'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-2123C"
+	],
+	"is_jouyou": false
+};
+//U-2008E
+kanji_defs['𠂎'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-2008E"
+	],
+	"is_jouyou": false
+};
+//U-20675
+kanji_defs['𠙵'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-20675"
+	],
+	"is_jouyou": false
+};
+//U-20695
+kanji_defs['𠚕'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-20695"
+	],
+	"is_jouyou": false
+};
+//U-2020C
+kanji_defs['𠈌'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-2020C"
+	],
+	"is_jouyou": false
+};
+//U+31C9
+kanji_defs['㇉'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Stroke Szwg"
+	],
+	"is_jouyou": false
+};
+//U-3405
+kanji_defs['㐅'] = {
+	"on_readings": [],
+	"kun_readings": [],
+	"meanings": [
+		"CJK Unified Ideograph-3405"
+	],
+	"is_jouyou": false
+};
