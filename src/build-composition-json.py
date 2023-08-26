afbbd80921b30adfddafd2e896020a8563154876
Uchiwake Kanji <uchiwakekanji@gmail.com>
2023-08-26 19:55:50 +0700

    Set uchiwake kanji as main user

--- src/build-composition-json.py
+++ src/build-composition-json.py
@@ -38,4 +38,3 @@ with io.open(output_file, "w", encoding='utf8') as fp:
     # Check ensure_ascii -> False (Print the kanji as is); True (print the kanji as unicode)
     print("INFO", "Clear browser cache to load new kjwk-composition.js", sep=" - ")
     json.dump(composition, fp, ensure_ascii=False)
-   
